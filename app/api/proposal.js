86f85811e1a1bd79326fd5518041c740c420a44d
Akhlaq <engr.akhlaq@hotmail.com>
2025-10-16 02:13:11 +0800

    fix: ensure panel wattage is included in API request parameters for business proposals

--- app/api/proposal.js
+++ app/api/proposal.js
@@ -4,9 +4,10 @@ const proposal = {
   getBusinessProposalByLocation({ lat, lng, billAmount }) {
     return api.get(`public_users/get_business_proposal_by_location_installers`, {
       params: {
-        lat,
-        lng,
-        billAmount,
+      lat,
+      lng,
+      billAmount,
+      panel_wattage: process.env.NEXT_PUBLIC_PANEL_WATTAGE,
       },
     });
   },
